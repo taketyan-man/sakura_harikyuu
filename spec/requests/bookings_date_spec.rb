a80c79a0f376af4d770fbaab93ab8fb0a4518c8a
taketyan-man <pbb4g7zhfb@yahoo.co.jp>
2024-05-18 23:53:07 +0900

    Booking_date controller rspec

--- spec/requests/bookings_date_spec.rb
+++ spec/requests/bookings_date_spec.rb
@@ -3,4 +3,13 @@ requrire 'rails_helper'
 RSpec.describe "BookingDates", type: :request do
   describe 'GET /index' do
   end
+  describe 'GET /new' do
+  end
+  describe 'POST /create' do
+  end
+  describe 'GET /show' do
+  end
+
+  describe 'POST /delete' do
+  end
 end
