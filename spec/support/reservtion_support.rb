b0a0bf256fd4cbb003800f6b9af03fa889ec2afd
taketyan-man <pbb4g7zhfb@yahoo.co.jp>
2024-05-31 23:43:21 +0900

    homepage reiout

--- spec/support/reservtion_support.rb
+++ spec/support/reservtion_support.rb
@@ -1,13 +1,9 @@
 module ReservationSupport
   module System
-    def reservation(booking)
-      visit booking_date_path
-      click_link   '○'
-      fill_in      '名前',     with: booking.name
-      fill_in      '電話番号', with: booking.tell
-      select       'ボディケア 90分', from: 'メニュー'
-      select       'なし',            from: 'オプション'
-      click_button '予約する'
+    def booking_new_rand()
+      row_rand = rand(1..27)
+      row = all('tbody tr')[row_rand]
+      row.all('a', text: '○')[0].click
     end
   end
 
--- spec/system/booking_date_spec.rb
+++ spec/system/booking_date_spec.rb
@@ -2,9 +2,21 @@ require "rails_helper"
 
 RSpec.describe "booking_dates", type: :system do
   before do
-    driven_by(:capybara)
+    driven_by(:selenium_chrome_headless)
   end
 
-  describe "GET /booking_date" do 
+  describe "GET /booking_date" do
+    before do
+      visit booking_date_path
+    end
+
+    it "should get booking_date_new_path correct information" do
+      row = all('tbody tr')[1]
+      row.all('a', text: '○')[0].click
+      day_value = find('#booking_date_day').value
+      time_value = find('#booking_date_time').value
+      expect(day_value).to eq("#{Date.tomorrow.to_s}")
+      expect(time_value).to eq("8:00")
+    end
   end
 end
