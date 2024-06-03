521ad65e3fed1c2f63eb3782a6951775f288482c
taketyan-man <pbb4g7zhfb@yahoo.co.jp>
2024-06-03 19:28:31 +0900

    booking_date_show rspec

--- spec/system/booking_date_spec.rb
+++ spec/system/booking_date_spec.rb
@@ -68,7 +68,7 @@ RSpec.describe "booking_dates", type: :system do
       end
 
       it 'should not save double booking' do
-        reservation
+        reservation()
         visit booking_date_new_path(day: "#{Date.tomorrow.to_s}", time: "8:00")
         fill_in 'booking_date_name', with: 'テスト'
         fill_in 'booking_date_tell', with: '0' * 11
@@ -86,6 +86,62 @@ RSpec.describe "booking_dates", type: :system do
   describe 'GET /booking_date_show' do
     before do
       reservation()
+    end
+
+    context 'correct information' do
+      it 'should redirect booking_date_path' do
+        click_link '完了'
+        expect(page.body).to include('予約画面', '2ヶ月先まで予約することができます。')
+      end
+
+      it 'should show and not show delete-box if delete-btn click ' do
+        expect(page).to have_css('div.delete-box', visible: false)
+        delete_btn     = find('a[id=delete-btn]')
+        delete_btn.click
+        expect(page).to have_css('div.delete-box', visible: true)
+        delete_out_btn = find('a[id=delete-content-out]')
+        delete_out_btn.click
+        expect(page).to have_css('div.delete-box', visible: false)
+      end
+
+      it 'should delete booking_date_delete_path' do
+        delete_btn = find('a[id=delete-btn]')
+        delete_btn.click
+        deletebtn = find('input[id=host_button]')
+        checkbox = find('#delete_check')
+        checkbox.check
+        fill_in 'name', with: 'テスト'
+        fill_in 'tell', with: "0" * 11
+
+        deletebtn.click
+        expect(page.body).to include('予約画面')
+      end
+    end
+
+    context 'incorrect information' do
+      it 'should not delete if not check check-box' do
+        delete_btn = find('a[id=delete-btn]')
+        delete_btn.click
+        deletebtn = find('input[id=host_button]')
+        fill_in 'name', with: 'テスト'
+        fill_in 'tell', with: "0" * 11
+
+        deletebtn.click
+        expect(page).to have_css('div.delete-box', visible: true)
+      end
+
+      it 'shold not delete with incorrect information' do
+        delete_btn = find('a[id=delete-btn]')
+        delete_btn.click
+        deletebtn = find('input[id=host_button]')
+        checkbox = find('#delete_check')
+        checkbox.check
+        fill_in 'name', with: ''
+        fill_in 'tell', with: "0" * 11
+
+        deletebtn.click
+        expect(page.body).to include('以下のエラーが発生しました', '確認用に入力していたものが違います。', 'もう一度最初から入力してください。')
+      end
     end
   end
 end
